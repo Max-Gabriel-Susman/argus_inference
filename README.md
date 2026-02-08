5737d1836ae0cb688bd38ca69c122003ec97f550
Max-Gabriel-Susman <gabe.susman@gmail.com>
2026-02-08 06:57:44 -0700

    resolved CI issues

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Argus Inference
 
 This repository contains the inference package for the Argus perception pipeline
+
+## Development 
+
+Run tests locally by navigating to the workspace directory for this package:
+```
+colcon test --packages-select argus_inference
+```
+
+Then output the results like so:
+```
+colcon test-result --verbose
+```
--- argus_inference/inference_node.py
+++ argus_inference/inference_node.py
@@ -3,20 +3,22 @@ from rclpy.node import Node
 
 
 class InferenceNode(Node):
-  def __init__(self):
-    super().__init__("argus_inference")
-    self.get_logger().info("argus_inference online")
+    def __init__(self):
+        super().__init__('argus_inference')
+        self.get_logger().info('argus_inference online')
+
 
 def main():
-  rclpy.init()
-  node = InferenceNode()
-  try:
-      rclpy.spin(node)
-  except KeyboardInterrupt:
-    pass
-  finally:
-    node.destroy_node()
-    rclpy.shutdown()
+    rclpy.init()
+    node = InferenceNode()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
 
-if __name__ == "__main__": 
-  main()
+
+if __name__ == '__main__':
+    main()
